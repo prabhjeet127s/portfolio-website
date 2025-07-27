de17f3ec75f00bf83232c940750baa6d2cad71be
prabhjet singh <prabhjeet127.s@gmail.com>
2025-07-27 23:44:31 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from "./components/Nav"
+
+
+function App() {
+ 
+
+  return (
+  
+
+    <div className=" bg-[#171d32] h-auto w-full overflow-hidden">
+
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20'></nav>
+  )
+}
+
+export default Nav
