a8df7153688b5eb5f13c0c7ccc48e3bef019b4fe
prabhjet singh <prabhjeet127.s@gmail.com>
2025-07-29 16:12:26 +0530

    created responsive navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import React from 'react'
+import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
 
 const Nav = () => {
 
@@ -10,7 +11,7 @@ const Nav = () => {
 
       <span className='text-xl font-bold tracking-wide'>Portfolio</span>
 
-      <ul className={'${menu?:"block:"hidden}    mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6'}>
+      <ul className={`${menu?"block":"hidden"}    mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
         <a href="#About">
         <li className='text-md transition-all duration-300 p-1 md:p-0'>About</li>
         </a>
@@ -28,6 +29,21 @@ const Nav = () => {
         </a>
 
       </ul>
+       {showMenu ? (
+        <RiMenu2Line
+          size={30}
+          className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          onClick={() => {
+            openMenu(!menu);
+            setShowmenu(!showMenu);
+          }}
+        />
+      ) : (
+        <RiCloseLine
+          size={30}
+          className="md:hidden absolute right-10 top-6 transition-all duration-300"
+        />
+      )}
     </nav>
   )
 }
